82a83f2a0cea4280166cae0b8056e5172e692a69
zuisho-1848-o <mizuki.hamada1848@gmail.com>
2022-08-26 15:58:33 +0900

    ハンバーガーメニューを実装

--- docs/main.js
+++ docs/main.js
@@ -1,1 +1,5 @@
 console.log("loaded!");
+const hamburgerBtn = document.getElementById("hamburgerBtn");
+hamburgerBtn.addEventListener("click", (e) => {
+    document.querySelector("header nav ul").classList.toggle("active");
+})
