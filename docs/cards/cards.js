11bb321d0ecbf30f2f477215d0b4a51d9f7fd4e9
zuisho-1848-o <mizuki.hamada1848@gmail.com>
2022-11-22 08:21:18 +0900

    カードクリック時の拡大を実装

--- docs/cards/cards.js
+++ docs/cards/cards.js
@@ -1,5 +1,6 @@
 const selectLis = [...document.querySelectorAll("#selectShow ul li")];
 const cardsShows = [...document.querySelectorAll(".cardsShow")];
+const cardExpansion = document.getElementById("cardExpansion");
 
 selectLis.forEach(li => li.addEventListener("click", (e) => {
     selectLis.forEach(allLi => allLi.classList.remove("is_active"));
@@ -9,7 +10,17 @@ selectLis.forEach(li => li.addEventListener("click", (e) => {
     document.getElementById(e.target.id + "Cards").classList.add("is_show");
 }))
 
+const cardImages = [...document.querySelectorAll(".cardsShow img")];
+cardImages.forEach(elem => elem.addEventListener("click", (e) => {
+    cardExpansion.classList.add("is_visible");
+    console.log(e.target);
+    cardExpansion.insertAdjacentElement("beforeend", e.target.cloneNode());
+}))
 
+cardExpansion.addEventListener("click", (e) => {
+    e.target.classList.remove("is_visible");
+    cardExpansion.innerHTML = "";
+})
 
 
 
